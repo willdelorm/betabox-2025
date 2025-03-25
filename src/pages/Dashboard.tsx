5a87adba968e3b20da89a9f0a738f41d45767c82
Will Delorm <willdelorm@gmail.com>
2025-03-24 21:46:15 -0700

    structure dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,19 @@
-import { handleLogout } from "../utils/auth.utils";
+import { Link } from "react-router";
+import BtnFooter from "../components/BtnFooter";
+import Nav from "../components/Nav";
 
 function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to the dashboard</p>
-      <button
-        className="w-full bg-primary text-white p-3 rounded-lg hover:brightness-110 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        onClick={() => handleLogout()}>
-        Log out
-      </button>
-    </div>
+    <>
+      <Nav />
+      <main className="flex-1">
+        <h1>Dashboard</h1>
+        <p>Welcome to the dashboard</p>
+      </main>
+      <Link to="/">
+        <BtnFooter>Start a Session</BtnFooter>
+      </Link>
+    </>
   );
 }
 
